eadf7e21c5b8ae80b8a869b927dcb2c71221f44b
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-10-27 14:54:37 -0700

    Add resource not found exception

--- app/Services/AuthorizationService.js
+++ app/Services/AuthorizationService.js
@@ -1,7 +1,12 @@
+const ResourceNotFoundException = use("App/Exceptions/ResourceNotFoundException");
 const InvalidAccessException = use("App/Exceptions/InvalidAccessException");
 
 class AuthorizationService {
   verifyPermission(resource, user) {
+    if (!resource) {
+      throw new ResourceNotFoundException();
+    }
+
     if (resource.user_id != user.id) {
       throw new InvalidAccessException();
     }
